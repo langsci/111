ded4a010ae0e6ae6fc4694314cb08204dbf713ec
Sebastian Nordhoff <sebastian.nordhoff@glottotopia.de>
2017-05-15 15:00:40 +0200

    new class

--- autoindex.py
+++ autoindex.py
@@ -9,34 +9,23 @@ print("found %i language names for autoindexing" % len(lgs))
 print("found %i subject terms for autoindexing" % len(terms))
 
 files = glob.glob('chapters/*tex')
-
-SUBJECTP = re.compile
 for f in files:
   print("indexing %s" % f)
-  #strip preamble of edited volume chapters to avoid indexing there
-  a = open(f).read().split(r"\begin{document}")  
-  content = a[-1]
-  preamble = ''
-  joiner = ''
-  if len(a) == 2:
-    preamble = a[0]
-    joiner = r"\begin{document}"
+  c = open(f).read()  
   for lg in lgs:
     lg = lg.strip()
     if lg == '':
       continue
-    content= re.sub('(?<!ili{)%s(?![\w}])'%lg, '\ili{%s}'%lg, content)
+    c = re.sub('(?<!ili{)%s(?![\w}])'%lg, '\ili{%s}'%lg, c)
   for term in terms:
-    term = term.strip() 
+    term = term.strip()
     if term == '':
        continue
-    content= re.sub('(?<!isi{)%s(?![-_\w}])'%term, '\isi{%s}'%term, content)
-  nlg = len(re.findall('\\ili{',content))
-  nt = len(re.findall('\\isi{',content))
+    c = re.sub('(?<!isi{)%s(?![\w}])'%term, '\isi{%s}'%term, c)
+  nlg = len(re.findall('\\ili{',c))
+  nt = len(re.findall('\\isi{',c))
   outfile = open(f.replace('chapters','indexed'), 'w')
-  outfile.write(preamble)
-  outfile.write(joiner)
-  outfile.write(content)
+  outfile.write(c)
   outfile.close()
   print(" %s now contains %i indexed languages and %i indexed subject terms"%(f.split('/')[-1],nlg,nt))
   
